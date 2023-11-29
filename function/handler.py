ded29962bed16a2515c4f056b845971db204e2cf
ldur <lasse@durucz.com>
2023-11-29 10:44:39 +0100

    last local changes

--- function/handler.py
+++ function/handler.py
@@ -1,12 +1,20 @@
 from datetime import datetime
 
-def handler(event, context):
+def days_until_christmas():
     
-    date1 = datetime.now().date()
-    date2 = datetime(2023, 12, 24)
-    delta = date2 - date1
+    today = datetime.now().date()
+    christmas = datetime(today.year, 12, 24).date()
 
+    if today > christmas:
+       christmas = datetime(today.year + 1, 12, 24).date()
+
+    delta = christmas - today
+    days = str(delta.days) + " days until Christmas!"
+    return days
+
+
+def handler(event, context):
     return {
         'statusCode': 200,
-        'body': str(delta.days)
+        'body':days_until_christmas()
     }
