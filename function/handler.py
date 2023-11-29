eecffb75b912b2facf453fdac7ac7f600bb90a5e
ldur <lasse@durucz.com>
2023-11-29 09:38:38 +0100

    Update handler.py

--- function/handler.py
+++ function/handler.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 
 def handler(event, context):
+    
+    date1 = datetime.now().date()
+    date2 = datetime(2023, 12, 24)
+    delta = date2 - date1
+
     return {
         'statusCode': 200,
-        'body': datetime.now().isoformat(),
-        'h1': "Lasse er pen"
+        'body': str(delta.days)
     }
-
